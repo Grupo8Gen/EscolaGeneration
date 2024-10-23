5aa21c4e7863acc73246e1d493eea2f314a5b3b7
Michelle Madeira <michellemadeira1@gmail.com>
2024-10-23 07:41:18 -0300

    Duplicidade

--- src/main/java/com/generation/sistema_escola/repository/FuncionarioRepository.java
+++ src/main/java/com/generation/sistema_escola/repository/FuncionarioRepository.java
@@ -3,8 +3,6 @@ package com.generation.sistema_escola.repository;
 import com.generation.sistema_escola.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
     boolean existsByEmail(String email);
 }
